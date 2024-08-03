80a3bea5eabb227ca1e6ad51c95006472f7849b7
mohamad khawam <khawammohamad99@gmail.com>
2024-08-03 14:08:44 -0400

    Fixed error on req.ip buffer size

--- src/routes/user/utils/userUtils.ts
+++ src/routes/user/utils/userUtils.ts
@@ -4,7 +4,11 @@ import crypto from "crypto";
 import { log } from "../../../utils/output/debug";
 function validateUser(req: AuthenticatedRequest, res: Response, next: NextFunction) {
     if (!req.client) return res.status(401).json({ status: "Unauthorized" });
-    if (!req.ip || !crypto.timingSafeEqual(Buffer.from(req.ip), Buffer.from((req.client as beaconToken).ip))) {
+    if (
+        !req.ip ||
+        Buffer.from(req.ip).length != Buffer.from((req.client as beaconToken).ip).length ||
+        !crypto.timingSafeEqual(Buffer.from(req.ip), Buffer.from((req.client as beaconToken).ip))
+    ) {
         log(`Ip for user does not match WANTED ${req.client.ip} GOT ${req.ip}, user token used from different ip from registered`, "error");
 
         return res.status(400).json({ status: "Invalid Ip" });
